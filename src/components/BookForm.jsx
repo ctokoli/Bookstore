f795b1c51ddf55a2a6edc46e03c20a56cbd135b1
ctokoli <matumasie@gmail.com>
2023-07-21 18:22:01 +0000

    Update BookForm.jsx

---
+++ src/components/BookForm.jsx
@@ -0,0 +1,11 @@
+const BookForm = () => {
+    return ( 
+        <form>
+            <input type="input" placeholder="Add book" />
+            <input type="input" placeholder="Add Author" />
+            <button type="submit">Add Book</button>
+        </form>
+     );
+}
+ 
+export default BookForm;
